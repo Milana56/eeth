2d339b276c0d5fe9d60543b50d7de313e178b41c
Edgar Eduardo <edgardeveloper@gmail.com>
2021-03-23 21:06:09 -0600

    imagesReady

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,7 +3,7 @@ export const portfolio =[
         "id":"ID1",
         "name":"Contact Form",
         "description":"Contact form created with React and Bootstrap. Using reCAPTCHA and operations to prevent bots.",
-        "image":"./src/sources/images/ContactForm.jpg",
+        "image":"https://i.imgur.com/CUbGCSP.jpg",
         "video":"http://www.youtube.com/embed/jhHfaON9OGQ",
         "github":".."
     },
@@ -11,7 +11,7 @@ export const portfolio =[
         "id":"ID2",
         "name":"Project Golang",
         "description":"Web site created with Golang, PostgreSQL, Javascript and other technologies. Proving CRUD, Sign In and Log Out, different rolls, modals and web sockets",
-        "image":"./public/assets/images/projects/Project-Golang.jpg",
+        "image":"https://i.imgur.com/le4wEKD.jpg",
         "video":"http://www.youtube.com/embed/oSF41Gj0nKw",
         "github":".."
     },
@@ -19,7 +19,7 @@ export const portfolio =[
         "id":"ID3",
         "name":"Hnos",
         "description":"A simple web wite created with React, CSS3 and Bootstrap. Project that just show templates, using hooks. Nice start with the framework.",
-        "image":"./assets/images/projects/Hnos.jpg",
+        "image":"https://i.imgur.com/DoOUGEt.jpg",
         "video":"http://www.youtube.com/embed/hGXh9X2KL4Y",
         "github":".."
     },
@@ -27,7 +27,7 @@ export const portfolio =[
         "id":"ID4",
         "name":"Tacones Mina Web",
         "description":"Web site to my friend Christopher created with Python using flask, Bootstrap, CSS3, MongoDB, fpdf and other technologies. Proving sell high heel shoes using PayPal.",
-        "image":"./assets/images/projects/Tacones-Mina-Web.jpg",
+        "image":"https://i.imgur.com/zXCxVqh.jpg",
         "video":"http://www.youtube.com/embed/-dL8uBhzAvQ",
         "github":".."
     },
@@ -35,7 +35,7 @@ export const portfolio =[
         "id":"ID5",
         "name":"Tacones Mina Software",
         "description":"A Dekstop Software to control, register products, delete, update, supervising orders, and print documents from 'Tacones Mina Web' to my friend Christopher. Created with Python using Tkinter, Fpdf, Pymongo, Mongodb, Gunicorn, Wtforms and other technologies.",
-        "image":"./assets/images/projects/Tacones-Mina-Software.jpg",
+        "image":"https://i.imgur.com/YzXUfhR.jpg",
         "video":"http://www.youtube.com/embed/9ZH1K-1vB1w",
         "github":".."
     },
@@ -43,7 +43,7 @@ export const portfolio =[
         "id":"ID6",
         "name":"Recognition files",
         "description":"A Desktop Software created with Python using Tkinter. It Recognizes files that can be changed order and names, perfect to rename many files in a specific route in your computer or memory, remove names that do not follow a sequence and re-name all files in numerical order.",
-        "image":"./assets/images/projects/Recognition-files.jpg",
+        "image":"https://i.imgur.com/axZvFRx.jpg",
         "video":"http://www.youtube.com/embed/MzhEqWHiZDY",
         "github":".."
     },
@@ -51,7 +51,7 @@ export const portfolio =[
         "id":"ID7",
         "name":"Software Tkinter Operations",
         "description":"A Desktop Software created with Python using Tkinter. It shows a menu with different options, tables, phrases and zodiac signs using date.",
-        "image":"./assets/images/projects/Software-Tkinter-Operations.jpg",
+        "image":"https://i.imgur.com/jGckSSO.jpg",
         "video":"http://www.youtube.com/embed/nrjVga_nv6Q",
         "github":".."
     },
@@ -59,7 +59,7 @@ export const portfolio =[
         "id":"ID8",
         "name":"Recognition pattern of digits",
         "description":"A Desktop Software created with Python using Tkinter. It was a task from my university to understand one part of artificial intelligence. You can click the buttons on the screen, then, the program show you numbers that have the same pattern, you can deselect and clear all buttons that you clicked.",
-        "image":"./assets/images/projects/Recognition-pattern-of-digits.jpg",
+        "image":"https://i.imgur.com/ClGcnIp.jpg",
         "video":"http://www.youtube.com/embed/gt7AuZUTc7g",
         "github":".."
     },
@@ -67,7 +67,7 @@ export const portfolio =[
         "id":"ID9",
         "name":"Deligurth",
         "description":"A simple Web Site from my university created with PHP using CSS and MySql. Proving CRUD.",
-        "image":"./assets/images/projects/Deligurth.jpg",
+        "image":"https://i.imgur.com/A1V4GVd.jpg",
         "video":"http://www.youtube.com/embed/1Rl-YyAI9bY",
         "github":".."
     },
@@ -75,7 +75,7 @@ export const portfolio =[
         "id":"ID10",
         "name":"Music Library",
         "description":"A simple Web Site from my university created with PHP using CSS and MySql. It Shows information and music.",
-        "image":"./assets/images/projects/Music-Library.jpg",
+        "image":"https://i.imgur.com/AdQZegg.jpg",
         "video":"http://www.youtube.com/embed/n9OvIc0Yq3A",
         "github":".."
     },
@@ -83,7 +83,7 @@ export const portfolio =[
         "id":"ID11",
         "name":"Friends Library",
         "description":"A simple Web Site from my university created with PHP using CSS and MySql. It Shows information.",
-        "image":"./assets/images/projects/Friends-Library.jpg",
+        "image":"https://i.imgur.com/8DjX02j.jpg",
         "video":"http://www.youtube.com/embed/mXDy9c8-dDo",
         "github":".."
     },
